57200599f7c8854ba3d4499a1e9f539840d10760
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-05-06 08:07:19 -0300

    server nlw return

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapters";
+
+// const transport = nodemailer.createTransport({
+//   host: "smtp.mailtrap.io",
+//   port: 2525,
+//   auth: {
+//     user: "ebf04edda43d1d",
+//     pass: "efe85ac97284ad"
+//   }
+// });
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail(data: SendMailData) {
+
+  };
+}
+
+// async sendEmail({subject, body}: SendMailData){
+//   await transport.sendMail({
+//   from: 'Equipe Feedget <oi@feeedget.com>',
+//   to: 'Lucas Gomes <lgomes.lg99@gmail.com>',
+//   subject,
+//   html: body
+// });
+// }
+
+// 1.28 horas
